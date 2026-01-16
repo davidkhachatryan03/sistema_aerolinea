c57d01c82eeb229c3aeec399c4a5aa931338e5a5
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-16 11:19:47 -0300

    refactor: tipar las variables faltantes

--- init_db.py
+++ init_db.py
@@ -1,22 +1,27 @@
 from DBManager import DBManager
 from TriggerManager import *
 from mysql.connector import Error
+from mysql.connector.connection import MySQLConnection
+from mysql.connector.cursor import MySQLCursor
+from typing import Any, cast
 import os
 
 def main():
     try:
         db_manager = DBManager()
+        db_manager.conectar()
 
-        cursor, conexion = db_manager.conectar()
+        cursor: MySQLCursor | None = cast(MySQLCursor, db_manager.obtener_cursor())
+        conexion: MySQLConnection | None = cast(MySQLConnection, db_manager.obtener_conexion())
 
-        dir_actual = os.getcwd()
-        dir_sql = os.path.join(dir_actual, "sql")
+        dir_actual: str = os.getcwd()
+        dir_sql: str = os.path.join(dir_actual, "sql")
 
         db_manager.ejecutar_archivo_sql(os.path.join(dir_sql, "crear_db.sql"))
 
         cursor.execute("SHOW TABLES")
 
-        tablas = cursor.fetchall()
+        tablas: list[tuple] = cursor.fetchall()
 
         for tabla in tablas:
             if tabla[0] not in ["historial_cambios", "sys_config"]:
@@ -25,8 +30,8 @@ def main():
                 trigger_manager.crear_trigger_after_update()
                 trigger_manager.crear_trigger_after_delete()
 
-        dir_sql_inserts = os.path.join(dir_sql, "inserts")
-        archivos_dir_sql_inserts = sorted(os.listdir(dir_sql_inserts))
+        dir_sql_inserts: str = os.path.join(dir_sql, "inserts")
+        archivos_dir_sql_inserts: list[str] = sorted(os.listdir(dir_sql_inserts))
 
         for archivo in archivos_dir_sql_inserts:
             db_manager.ejecutar_archivo_sql(os.path.join(dir_sql_inserts, archivo))
