2228df35e2a882e7f71750dcc1b7c64e955a9d9f
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-20 13:28:57 -0300

    feat: permite registrar y modificar certificaciones de staff

--- src/CertificacionesStaffManager.py
+++ src/CertificacionesStaffManager.py
@@ -1,6 +1,9 @@
 from src.entidades.CertificacionStaff import CertificacionStaff
 from src.TablaManager import TablaManager
+from datetime import datetime
 from typing import Any
+
+FilaCertificacion = tuple[int, str, datetime, int | None]
 
 class CertificacionesStaffManager(TablaManager):
 
@@ -18,6 +21,51 @@ class CertificacionesStaffManager(TablaManager):
         datos: dict[str, Any] = certificacion.to_dict()
         
         super().agregar_fila(id_staff, datos)
+
+    def modificar_id_staff(self, id_certificacion: int, id_staff_modifica: int, id_staff_nuevo: int) -> None:
+        if not super()._verificar_id_a_modificar(id_certificacion):
+            raise Exception("Error: el id a modificar no existe.")
+
+        if not super()._verificar_id_staff(id_staff_modifica):
+            raise Exception("Error: el staff ingresado no es válido.")
+        
+        if not super()._verificar_id_staff(id_staff_nuevo):
+            raise Exception("Error: el staff ingresado no es válido.")
+        
+        certificacion: CertificacionStaff = self._obtener_documento(id_certificacion)
+
+        if certificacion.id_staff == id_staff_nuevo:
+            return
+        
+        super().modificar_fila(id_certificacion, id_staff_modifica, id_staff=id_staff_nuevo)
+
+    def modificar_descripcion(self, id_certificacion: int, id_staff: int, descripcion: str) -> None:
+        if not super()._verificar_id_a_modificar(id_certificacion):
+            raise Exception("Error: el id a modificar no existe.")
+
+        if not super()._verificar_id_staff(id_staff):
+            raise Exception("Error: el staff ingresado no es válido.")
+        
+        certificacion: CertificacionStaff = self._obtener_documento(id_certificacion)
+
+        if certificacion.id_staff == id_staff:
+            return
+        
+        super().modificar_fila(id_certificacion, id_staff, descripcion=descripcion)
+        
+    def modificar_vencimiento(self, id_certificacion: int, id_staff: int, licencia_hasta: datetime) -> None:
+        if not super()._verificar_id_a_modificar(id_certificacion):
+            raise Exception("Error: el id a modificar no existe.")
+
+        if not super()._verificar_id_staff(id_staff):
+            raise Exception("Error: el staff ingresado no es válido.")
+        
+        certificacion: CertificacionStaff = self._obtener_documento(id_certificacion)
+
+        if certificacion.licencia_hasta == licencia_hasta:
+            return
+        
+        super().modificar_fila(id_certificacion, id_staff, licencia_hasta=licencia_hasta)
     
     def _verificar_campos_requeridos(self, certificacion: CertificacionStaff):
         for campo in self.campos_requeridos:
@@ -25,3 +73,23 @@ class CertificacionesStaffManager(TablaManager):
                 return False
         
         return True
+    
+    def _obtener_documento(self, id_certificacion: int) -> CertificacionStaff:
+        query = """
+                SELECT  id_staff,
+                        descripcion,
+                        licencia_hasta,
+                        id
+                FROM    certificaciones_staff
+                WHERE   id = %s
+                """
+        
+        consulta_certificacion: list[tuple] = self.db_manager.consultar(query, (id_certificacion,))
+
+        if consulta_certificacion:
+            fila_certificacion: FilaCertificacion = consulta_certificacion[0]
+            certificacion = CertificacionStaff(*fila_certificacion)
+        else:
+            raise Exception("Error: no se encontró ningún resultado al consultar.")
+        
+        return certificacion
