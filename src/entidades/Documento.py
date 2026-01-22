abb7266fea139d4d266a5ca9d20936e02e9746b2
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-22 14:41:30 -0300

    refactor: permitir crear documentos con filas ya existentes

--- src/entidades/Documento.py
+++ src/entidades/Documento.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 from typing import Any
 
-class Documento:
+class DocumentoBase:
 
-    def __init__(self, num_documento: str, fecha_vencimiento: datetime, pais_emision: str, id_pasajero: int, id_tipo_documento: int, id: int | None) -> None:
+    def __init__(self, num_documento: str, fecha_vencimiento: datetime, pais_emision: str, id_pasajero: int, id_tipo_documento: int) -> None:
         self.num_documento = num_documento
         self.fecha_vencimiento = fecha_vencimiento
         self.pais_emision = pais_emision
         self.id_pasajero = id_pasajero
         self.id_tipo_documento = id_tipo_documento
-        self.id = id
 
     def to_dict(self) -> dict[str, Any]:
         datos = {
@@ -17,8 +16,25 @@ class Documento:
             "fecha_vencimiento": self.fecha_vencimiento,
             "pais_emision": self.pais_emision,
             "id_pasajero": self.id_pasajero,
-            "id_tipo_documento": self.id_tipo_documento,
-            "id": self.id
+            "id_tipo_documento": self.id_tipo_documento
+        }
+
+        return datos
+
+class DocumentoDesdeDB(DocumentoBase):
+
+    def __init__(self, id: int, num_documento: str, fecha_vencimiento: datetime, pais_emision: str, id_pasajero: int, id_tipo_documento: int) -> None:
+        super().__init__(num_documento, fecha_vencimiento, pais_emision, id_pasajero, id_tipo_documento)
+        self.id = id
+
+    def to_dict(self) -> dict[str, Any]:
+        datos = {
+            "id": self.id,
+            "num_documento": self.num_documento,
+            "fecha_vencimiento": self.fecha_vencimiento,
+            "pais_emision": self.pais_emision,
+            "id_pasajero": self.id_pasajero,
+            "id_tipo_documento": self.id_tipo_documento
         }
 
         return datos
