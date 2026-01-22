ce287b46530e2a7d03399e6b26ce0dff1544becc
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-22 14:33:46 -0300

    refactor: permitir crear tarjetas de embarque con filas ya existentes

--- src/entidades/TarjetaEmbarque.py
+++ src/entidades/TarjetaEmbarque.py
@@ -1,22 +1,35 @@
 from datetime import datetime
 from typing import Any
 
-class TarjetaEmbarque:
+class TarjetaEmbarqueBase:
 
-    def __init__(self, id_venta: int, id: int | None=None, fecha_emision: datetime | None=None, fecha_embarque: datetime | None=None, id_estado_actual: int | None=None) -> None:
-        self.id_venta = id_venta
-        self.id = id
-        self.fecha_emision = fecha_emision
-        self.fecha_embarque = fecha_embarque
+    def __init__(self, id_estado_actual: int, id_venta: int) -> None:
         self.id_estado_actual = id_estado_actual
+        self.id_venta = id_venta
 
     def to_dict(self) -> dict[str, Any]:
         datos = {
-            "id_venta": self.id_venta,
+            "id_estado_actual": self.id_estado_actual,
+            "id_venta": self.id_venta
+        }
+
+        return datos
+    
+class TarjetaEmbarqueDesdeDB(TarjetaEmbarqueBase):
+
+    def __init__(self, id: int, fecha_emision: datetime, fecha_embarque: datetime | None, id_estado_actual: int, id_venta: int) -> None:
+        super().__init__(id_estado_actual, id_venta)
+        self.id = id
+        self.fecha_emision = fecha_emision
+        self.fecha_embarque = fecha_embarque
+
+    def to_dict(self) -> dict[str, Any]:
+        datos = {
             "id": self.id,
             "fecha_emision": self.fecha_emision,
             "fecha_embarque": self.fecha_embarque,
-            "id_estado_actual": self.id_estado_actual
+            "id_estado_actual": self.id_estado_actual,
+            "id_venta": self.id_venta
         }
 
         return datos
