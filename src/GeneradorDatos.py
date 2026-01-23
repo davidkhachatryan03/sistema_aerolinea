57275121524364717df2aa5eaad147be747f5757
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-23 13:53:38 -0300

    refactor: reescritura de generar pasajeros con entidades nuevas

--- src/GeneradorDatos.py
+++ src/GeneradorDatos.py
@@ -11,8 +11,22 @@ class GeneradorDatos:
     def __init__(self) -> None:
         self.fake: Faker = Faker('es_AR')
 
-    def generar_pasajeros(self):
-        pass
+    def generar_pasajeros(self, cant: int) -> list[PasajeroBase]:
+        pasajeros: list[PasajeroBase] = []
+        
+        for _ in range(cant):
+            nombre_completo: str = self.fake.name()
+            email: str = self.fake.email()
+            telefono: str = self.fake.numerify("11%#######")
+            esta_en_lista_negra: bool = random.choices([True, False], weights=[3, 97])[0]
+            es_vip: bool = random.choices([True, False], weights=[20, 80])[0]
+
+            pasajero = PasajeroBase(nombre_completo, email, telefono, esta_en_lista_negra, es_vip)
+
+            pasajeros.append(pasajero)
+
+        return pasajeros
+
 
     def generar_ventas(self):
         pass
