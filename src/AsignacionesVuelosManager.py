3e45a8d1309d5f1e44d49296dc2b8c3f278be7ea
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-24 10:56:57 -0300

    feat: permitir la asignacion de tripulacion a vuelos

--- src/AsignacionesVuelosManager.py
+++ src/AsignacionesVuelosManager.py
@@ -2,7 +2,6 @@ from typing import Any
 from src.DBManager import DBManager
 from src.TablaManager import TablaManager
 
-# asumo cada vuelo necesita dos pilotos y 3 azafatas
 class AsignacionesVuelosManager(TablaManager):
 
     def __init__(self, db_manager: DBManager) -> None:
