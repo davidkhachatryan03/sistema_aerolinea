53f70c577c1f3f622ab78ac287febc4ca91d2d3b
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-01-27 12:15:46 -0300

    fix: corrección de tipado

--- src/entidades/AsignacionVuelo.py
+++ src/entidades/AsignacionVuelo.py
@@ -1,8 +1,9 @@
 from typing import Any
+from datetime import datetime
 
 class AsignacionVueloBase:
 
-    def __init__(self, fecha_inicio: int, fecha_fin: int, id_rol: int, id_vuelo: int, id_staff: int) -> None:
+    def __init__(self, fecha_inicio: datetime, fecha_fin: datetime, id_rol: int, id_vuelo: int, id_staff: int) -> None:
         self.fecha_inicio = fecha_inicio
         self.fecha_fin = fecha_fin
         self.id_rol = id_rol
@@ -23,7 +24,7 @@ class AsignacionVueloBase:
 
 class AsignacionVueloDesdeDB(AsignacionVueloBase):
 
-    def __init__(self, id: int, fecha_inicio: int, fecha_fin: int, id_rol: int, id_vuelo: int, id_staff: int) -> None:
+    def __init__(self, id: int, fecha_inicio: datetime, fecha_fin: datetime, id_rol: int, id_vuelo: int, id_staff: int) -> None:
         super().__init__(fecha_inicio, fecha_fin, id_rol, id_vuelo, id_staff)
         self.id = id
 
