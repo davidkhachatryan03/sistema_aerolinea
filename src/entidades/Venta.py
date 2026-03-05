8227b1ac93f8392f750fe9fa741b4e0bb5a85de1
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-03-05 13:20:30 -0300

    refactor: permitir validaciones de formato

--- src/entidades/Venta.py
+++ src/entidades/Venta.py
@@ -1,11 +1,12 @@
 from typing import Any
 from datetime import datetime
 from decimal import Decimal
+import re
 
 class VentaBase:
 
     def __init__(self, id_pasajero: int, id_vuelo: int, num_reserva: str, precio_pagado_usd: Decimal, id_estado_actual: int) -> None:
-        if type(id_pasajero) != int or type(id_vuelo) != int or type(num_reserva) != str or type(precio_pagado_usd) != Decimal or type(id_estado_actual) != int:
+        if not self._verificar_formato_id(id_pasajero) or not self._verificar_formato_id(id_vuelo) or not self._verificar_formato_id(id_estado_actual) or not self._verificar_formato_num_reserva(num_reserva) or not self._verificar_formato_precio_pagado_usd(precio_pagado_usd):
             raise Exception("Error: el formato de los datos es incorrecto.")
 
         self.id_pasajero = id_pasajero
@@ -24,6 +25,38 @@ class VentaBase:
         }
 
         return datos
+    
+    def _verificar_formato_id(self, id: int) -> bool:
+        if type(id) != int:
+            return False
+        
+        if id <= 0:
+            return False
+
+        return True
+    
+    def _verificar_formato_num_reserva(self, num_reserva: str) -> bool:
+        patron_num_reserva = r"^[A-K]{3}\d{3}$"
+
+        if type(num_reserva) != str:
+            return False
+        
+        if len(num_reserva) != 6:
+            return False
+        
+        if not re.match(patron_num_reserva, num_reserva):
+            return False
+        
+        return True
+    
+    def _verificar_formato_precio_pagado_usd(self, precio_pagado_usd: Decimal) -> bool:
+        if type(precio_pagado_usd) != Decimal:
+            return False
+        
+        if precio_pagado_usd < 0:
+            return False
+        
+        return True
 
 class VentaDesdeDB(VentaBase):
 
