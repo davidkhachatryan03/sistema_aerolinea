e85d1e4a4c7aac317c3da8fe92cfd291407b9c4c
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-03-11 09:42:25 -0300

    test: agregado de tests para managers de vuelos

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -347,3 +347,36 @@ def test_modificar_vuelo_fechas(db_conectada: DBManager):
     assert vuelo.id_ruta == id_ruta
     assert vuelo.id_avion == id_avion
     assert vuelo.id_estado_actual == id_estado_actual
+
+def test_modificar_vuelo_avion(db_conectada: DBManager):
+    vuelos_manager = VuelosManager(db_conectada)
+    ID_STAFF = 25
+
+    fecha_partida_programada = datetime(2026, 1, 1)
+    fecha_arribo_programada = datetime(2026, 1, 2)
+    id_ruta = random.choice(db_conectada.consultar_columna_unica("SELECT id FROM rutas LIMIT 10"))
+    id_avion = elegir_avion_valido(db_conectada, id_ruta, fecha_partida_programada, fecha_arribo_programada, vuelos_manager)
+    id_estado_actual = 1 # siempre se asigna este valor al registar un vuelo
+    costo_operativo_usd = vuelos_manager._calcular_costo_operativo_usd(id_ruta, id_avion)
+    precio_venta_usd = vuelos_manager._calcular_precio_venta_usd(costo_operativo_usd)
+
+    vuelo = VueloBase(id_ruta, id_avion, id_estado_actual, fecha_partida_programada, fecha_arribo_programada, costo_operativo_usd, precio_venta_usd)
+    vuelos_manager.registrar_vuelo(ID_STAFF, vuelo)
+
+    consulta_id_vuelo: list[int] = db_conectada.consultar_columna_unica("SELECT id FROM vuelos ORDER BY id DESC LIMIT 1")
+    id_vuelo = consulta_id_vuelo[0]
+
+    nuevo_id_avion = elegir_avion_valido(db_conectada, id_ruta, fecha_partida_programada, fecha_arribo_programada, vuelos_manager)
+
+    vuelos_manager.modificar_avion(id_vuelo, ID_STAFF, nuevo_id_avion)
+
+    consulta_vuelo: list[FilaVuelo] = db_conectada.consultar("SELECT id, fecha_partida_programada, fecha_arribo_programada, fecha_partida_real, fecha_arribo_real, costo_operativo_usd, precio_venta_usd, id_ruta, id_avion, id_estado_actual FROM vuelos ORDER BY id DESC LIMIT 1")
+    vuelo = VueloDesdeDB(*consulta_vuelo[0])
+
+    assert vuelo.fecha_partida_programada == fecha_partida_programada
+    assert vuelo.fecha_arribo_programada == fecha_arribo_programada
+    assert vuelo.costo_operativo_usd == costo_operativo_usd
+    assert vuelo.precio_venta_usd == precio_venta_usd
+    assert vuelo.id_ruta == id_ruta
+    assert vuelo.id_avion == nuevo_id_avion
+    assert vuelo.id_estado_actual == id_estado_actual
