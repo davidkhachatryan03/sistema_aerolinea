049c67f138c0ce0f6ff19ea7c1b6dc392f38c2b6
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-05-25 11:08:11 -0300

    refactor: delete unused files

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,3 +1,1 @@
 from src.common.db_manager import DBManager
-from src.common import queries
-from src.common import rows
--- src/common/rows.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-from decimal import Decimal
-
-BookingRow = tuple[int, str, datetime, Decimal, int]
