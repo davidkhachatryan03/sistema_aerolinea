843a918993835a31930cdb6ba43fcce4b1598ae8
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-06-01 12:50:46 -0300

    feat: add passenger repository

--- src/core/repositories/passenger_repository.py
+++ src/core/repositories/passenger_repository.py
@@ -1,6 +1,7 @@
 from src.common import DBManager
 from src.entities import Passenger
 from src.api.schemas import PassengerRequest
+from uuid import UUID
 
 class PassengerRepository:
 
@@ -9,3 +10,24 @@ class PassengerRepository:
 
     def insert_passengers(self, passengers: list[Passenger]) -> None:
         self.db_manager.insert_rows("passengers", passengers)
+
+    def retrieve_passengers(self, passengers_id: list[UUID]) -> list[Passenger]:
+        if not passengers_id:
+            return []
+        
+        placeholders = ",".join(["%s" * len(passengers_id)])
+
+        query = """
+                SELECT  *
+                FROM    passengers
+                WHERE   id IN ({})
+                """.format(placeholders)
+        
+        values = passengers_id
+
+        result: list[tuple] = self.db_manager.retrieve(query, values)
+
+        if result:
+            return [Passenger(*row) for row in result]
+        
+        return []
