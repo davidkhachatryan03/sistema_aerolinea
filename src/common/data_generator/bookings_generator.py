15aba9ec71463b1ecb2cf6d54b9c74dd8c11cb8f
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-06-02 09:04:35 -0300

    feat: add data seeder With one use case almost finished, it is necessary to test it properly by executing it on a simulated database.

--- src/common/data_generator/bookings_generator.py
+++
@@ -1,8 +0,0 @@
-from src.common.data_generator.data_generator import DataGenerator
-from src.entities import Ticket, Flight, Booking
-import string, random
-
-class BookingsGenerator(DataGenerator):
-
-    def __init__(self) -> None:
-        super().__init__()
--- src/common/data_generator/data_generator.py
+++
@@ -1,8 +0,0 @@
-from faker import Faker
-from src.common import DBManager
-
-class DataGenerator:
-    
-    def __init__(self) -> None:
-        self.fake = Faker("es_AR")
-        self.db_manager = DBManager()
