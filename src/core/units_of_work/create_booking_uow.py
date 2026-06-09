b89d2b57daa172d989f22cbe6332bf288ef65d18
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-06-09 19:25:19 -0300

    refactor: simplify initialization

--- src/core/units_of_work/create_booking_uow.py
+++ src/core/units_of_work/create_booking_uow.py
@@ -5,11 +5,11 @@ class CreateBookingUoW:
 
     def __init__(self, db_manager: DBManager) -> None:
         self.db_manager = db_manager
-        self.passenger_repository = PassengerRepository(self.db_manager)
-        self.document_repository = DocumentRepository(self.db_manager)
-        self.flight_repository = FlightRepository(self.db_manager)
-        self.booking_repository = BookingRepository(self.db_manager)
-        self.ticket_repository = TicketRepository(self.db_manager)
+        self.passenger_repository = PassengerRepository(db_manager)
+        self.document_repository = DocumentRepository(db_manager)
+        self.flight_repository = FlightRepository(db_manager)
+        self.booking_repository = BookingRepository(db_manager)
+        self.ticket_repository = TicketRepository(db_manager)
 
     def __enter__(self):
         self.db_manager.__enter__()
