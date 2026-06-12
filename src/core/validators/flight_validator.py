31367e57ca59bb3ed2d2517333dc04bfcfe84cde
David Khachatryan <david.v.khachatryan03@gmail.com>
2026-06-12 13:28:48 -0300

    fix: simplify flights existence validation This approach fixes a bug where the exception will not be raised if a flight is not registered.

--- src/core/validators/flight_validator.py
+++ src/core/validators/flight_validator.py
@@ -5,14 +5,8 @@ from src.entities import Flight
 class FlightValidator:
 
     def check_flights_existence(self, flights_requested: list[UUID], flights_retrieved: list[Flight]) -> None:
-        set_flights_requested = set(flights_requested)
-
-        if not flights_retrieved:
+        if len(flights_retrieved) != len(set(flights_requested)):
             raise InexistentFlight
-
-        for flight in flights_retrieved:
-            if flight.id not in set_flights_requested:
-                raise InexistentFlight
     
     def check_seats_available_per_flight(self, seats_available_per_flight: dict[UUID, int], number_of_passengers: int) -> None:
         for flight in seats_available_per_flight:
